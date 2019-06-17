07a53d7d7305c1d569843a27254cf839b2002746
Gastón Rial <grial1@correo.um.edu.uy>
2019-06-16 20:55:18 -0700

    Pending: not there yet, no classes implementation

---
+++ src/test/example.cc
@@ -0,0 +1,14 @@
+/// author Gastón Rial
+/// date 2019-06-12
+
+#include "InputDataset.h"
+
+using namespace std;
+namespace ma = model_api;
+
+int main(int argc, char** argv)
+{
+    ma::InputDataset oID;
+    ma::Dataset oD = oID;
+    return 0;
+}
